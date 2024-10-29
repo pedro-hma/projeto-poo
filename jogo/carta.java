d278a20fb37d9739097319e6f82ce5010561bd5a
pedro-hmayres <105680915+pedro-hma@users.noreply.github.com>
2024-10-29 16:27:34 +0000

    correção em carta

--- jogo/carta.java
+++ jogo/carta.java
@@ -1,8 +1,8 @@
-public class Carta {
+public class carta {
     private String naipe;
     private int valor;
 
-    public Carta(String naipe, int valor) {
+    public carta(String naipe, int valor) {
         this.naipe = naipe;
         this.valor = valor;
     }
